f3451ee3736f148536ff1c60e930a92864373cf1
tstiddy <spaceinvadini99@gmail.com>
2019-09-12 08:52:02 +0100

    fixing booking bug

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,11 +1,11 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :bookingDate, :bookingTime, :number_of_people, :restaurant_id
+  attributes :id, :booking_date, :booking_time, :number_of_people, :restaurant_id
 
-  def bookingDate
+  def booking_date
     object.booking_date.strftime("%d-%m-%Y")
   end
 
-  def bookingTime
+  def booking_time
     object.booking_time.strftime("%H:%M")
   end
 end
